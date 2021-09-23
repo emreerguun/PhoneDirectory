64ae169ae4e3b1b139e6f165f69e3f3740a561bc
Emre Ergün <emreergnn@hotmail.com>
2021-09-23 14:09:02 +0300

    Katmanlar oluşturuldu ve Entity ler hazırlandı.

---
+++ PhoneDirectory.Entities/Entities/ContactInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectory.Entities.Entities
+{
+    public class ContactInfo
+    {
+        public int ContactInfoID { get; set; }
+        public int InfoType { get; set; }
+        public string InfoContent { get; set; }
+
+
+        public int UserID { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ PhoneDirectory.Entities/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectory.Entities.Entities
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+
+        public List<ContactInfo> ContactInfos { get; set; }
+    }
+}
